ee53838c1bcaed8183bd4be7ee92c68f4a2a63c4
MatheoWintrebert <matheo.wintrebert@orange.fr>
2026-04-14 23:57:47 +0200

    refactor: :art: just format

--- src/infrastructure/decorators/public.decorator.ts
+++ src/infrastructure/decorators/public.decorator.ts
@@ -2,4 +2,6 @@ import { SetMetadata } from "@nestjs/common";
 import { IProtectionContext } from "@domain/types";
 
 export const Public = (): MethodDecorator & ClassDecorator =>
-  SetMetadata("protectionContext", { isPublic: true } satisfies IProtectionContext);
+  SetMetadata("protectionContext", {
+    isPublic: true,
+  } satisfies IProtectionContext);
